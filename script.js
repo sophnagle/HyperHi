7f86c606b0346ce6555f61fd4d658ea0473e7138
sophnagle <sophnagle@gmail.com>
2021-10-31 12:46:25 +0000

    Add mousemove javascript based on coordinates

--- script.js
+++ script.js
@@ -11,12 +11,27 @@ const shrinkCursor = function () {
     cursor.classList.remove("is-down");
 }
 
-// run fuction
+// move the cursor based on coordinates 
+const moveCursor = function (x, y) {
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+}
+
+
+// run fuctions
+// Grow the cursor on click 
 document.addEventListener("mousedown", function () {
     growCursor()
 })
 
+// shrink cursor off click 
 document.addEventListener("mouseup", function () {
     shrinkCursor()
 })
 
+// Move cursor based on coordinates 
+document.addEventListener("mousemove", function (event) {
+    // event.pageX -> where we are on the page across
+    // event.pageY -> where we are on the page downwards
+    moveCursor(event.pageX, event.pageY)
+})
